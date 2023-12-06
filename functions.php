036a486136366ed69c481422f4a8fa4e3c034874
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2023-12-06 12:15:05 +0100

    exercise CPT ajout type de post, taxonomies...

--- functions.php
+++ functions.php
@@ -6,4 +6,46 @@ function register_my_menu(){
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
 
-?>
+
+function velos_register_post_types() {
+
+    // CPT Velos 
+    $labels = array(
+        'name' => 'Velo',
+        'all_items' => 'Tous les velos',
+        'sigular_name' =>'velo',
+        'add_new_item' => 'Ajouter un velo',
+        'edit_item' => 'Modifier le velo',
+        'menu_name' => 'Velo'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show in rest' => true,
+        'has archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-admin-customizer',
+    );
+
+    register_post_type('velo', $args);
+
+    // Déclaration de la Taxonomie
+    $labels = array(
+      'name' => 'marque',
+      'new_item_name' => 'marque',
+      'parent_item' => 'velo',
+    );
+
+    $args = array( 
+      'labels' => $labels,
+      'public' => true, 
+      'show_in_rest' => true,
+      'hierarchical' => true, 
+  );
+
+    register_taxonomy('marque','velo', $args);
+
+  }
+add_action('init','velos_register_post_types');
