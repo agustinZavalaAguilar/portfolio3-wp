da03fdee540331efba0a7ad03c47f1e9f256bfd5
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2023-12-09 12:10:19 +0100

    php functions adding custom fields and taxonomies

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function register_my_menu(){
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
 
-
+/*
 function velos_register_post_types() {
 
     // CPT Velos 
@@ -49,3 +49,79 @@ function velos_register_post_types() {
 
   }
 add_action('init','velos_register_post_types');
+*/
+
+function apprenants2_register_post_types() {
+
+  // CPT apprenants2 
+  $labels = array(
+      'name' => 'apprenants2',
+      'all_items' => 'Tous les apprenants2',
+      'sigular_name' =>'apprenant2',
+      'add_new_item' => 'Ajouter un apprenant',
+      'edit_item' => 'Modifier apprenant',
+      'menu_name' => 'apprenants2',
+  );
+
+  $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'show in rest' => true,
+      'has archive' => true,
+      'supports' => array('title', 'editor', 'thumbnail'),
+      'menu_position' => 5,
+      'menu_icon' => 'dashicons-admin-customizer',
+  );
+
+  register_post_type('apprenants2', $args);
+
+  // Déclaration de la Taxonomie promo
+  $labels = array(
+    'name' => 'annee',
+    'new_item_name' => 'annee',
+    'parent_item' => 'apprenants2',
+  );
+
+  $args = array( 
+    'labels' => $labels,
+    'public' => true, 
+    'show_in_rest' => true,
+    'hierarchical' => true, 
+);
+
+  register_taxonomy('annee','apprenants2', $args);
+
+// Déclaration de la Taxonomie competence
+$labels = array(
+  'name' => 'competence',
+  'new_item_name' => 'competence',
+  'parent_item' => 'apprenants2',
+);
+
+$args = array( 
+  'labels' => $labels,
+  'public' => true, 
+  'show_in_rest' => true,
+  'hierarchical' => true, 
+);
+
+register_taxonomy('competence','apprenants2', $args);
+
+// Déclaration de la Taxonomie catégorie de formation
+$labels = array(
+  'name' => 'categorie',
+  'new_item_name' => 'categorie',
+  'parent_item' => 'apprenants2',
+);
+
+$args = array( 
+  'labels' => $labels,
+  'public' => true, 
+  'show_in_rest' => true,
+  'hierarchical' => true, 
+);
+
+register_taxonomy('categorie','apprenants2', $args);
+
+}
+add_action('init','apprenants2_register_post_types');
